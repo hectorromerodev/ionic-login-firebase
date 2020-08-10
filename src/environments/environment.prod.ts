d564e8a5959804e0c459b14c66f26f712d23d82e
hectorromerodev <hectorromero.developer@gmail.com>
2020-08-09 18:58:41 -0700

    ADD Firebase keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // Firebase keys
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLmu7iQLxpwi1buC5VVMIdK-gqo0VUaO4',
+    authDomain: 'login-example-8d0d1.firebaseapp.com',
+    databaseURL: 'https://login-example-8d0d1.firebaseio.com',
+    projectId: 'login-example-8d0d1',
+    storageBucket: 'login-example-8d0d1.appspot.com',
+    messagingSenderId: '232598078686',
+    appId: '1:232598078686:web:0daabaa411e75c6417ae4f'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Firebase keys
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLmu7iQLxpwi1buC5VVMIdK-gqo0VUaO4',
+    authDomain: 'login-example-8d0d1.firebaseapp.com',
+    databaseURL: 'https://login-example-8d0d1.firebaseio.com',
+    projectId: 'login-example-8d0d1',
+    storageBucket: 'login-example-8d0d1.appspot.com',
+    messagingSenderId: '232598078686',
+    appId: '1:232598078686:web:0daabaa411e75c6417ae4f'
+  }
 };
 
 /*
